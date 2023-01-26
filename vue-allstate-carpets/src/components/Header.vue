e3d96ebd62fc4d53ebfafb5c5e01cfb082040b0b
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-26 14:56:41 -0500

    added some components and did additional setup

---
+++ vue-allstate-carpets/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <div className="flex flex-row justify-center">AllStateCarpets</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderVue'
+}
+</script>
