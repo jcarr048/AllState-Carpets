a7a7edba5f9c4d705512547d02424f10f2b75c21
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-29 12:57:10 -0500

    added About Page with utility classes

--- vue-allstate-carpets/src/components/Header.vue
+++ vue-allstate-carpets/src/components/Header.vue
@@ -1,16 +1,18 @@
 <template>
   <header>
     <nav
-      class="flex flex-row px-10 align-middle bg-orange-200 bg-gradient-to-r from-amber-600"
+      class="flex flex-row align-middle bg-orange-200 bg-gradient-to-r from-amber-600"
     >
       <img src="../assets/aclogo.png" alt="logo" class="h-20 w-20" />
-      <div class="flex flex-row align-middle mx-5 my-auto px-5 justify-end">
-        <router-link to="/" class="mx-2 rounded-md py-1 text-red-900 font-bold"
+      <div
+        class="flex flex-row align-middle my-auto px-5 justify-items-stretch"
+      >
+        <router-link to="/" class="mx-2 rounded-md py-1 text-red-700 font-bold"
           >Home</router-link
         >
         <router-link
           to="/about"
-          class="mx-2 rounded-md py-1 text-red-900 font-bold justify-end"
+          class="mx-2 rounded-md py-1 text-red-700 font-bold justify-end"
           >About-Us</router-link
         >
       </div>
