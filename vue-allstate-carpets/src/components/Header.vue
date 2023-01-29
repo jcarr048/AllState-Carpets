f9cfafbd03e6880093d8416966c8513f4591b14a
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-29 12:34:46 -0500

    added navbar with utility classes

--- vue-allstate-carpets/src/components/Header.vue
+++ vue-allstate-carpets/src/components/Header.vue
@@ -1,6 +1,20 @@
 <template>
   <header>
-    <div className="flex flex-row justify-center">AllStateCarpets</div>
+    <nav
+      class="flex flex-row px-10 align-middle bg-orange-200 bg-gradient-to-r from-amber-600"
+    >
+      <img src="../assets/aclogo.png" alt="logo" class="h-20 w-20" />
+      <div class="flex flex-row align-middle mx-5 my-auto px-5 justify-end">
+        <router-link to="/" class="mx-2 rounded-md py-1 text-red-900 font-bold"
+          >Home</router-link
+        >
+        <router-link
+          to="/about"
+          class="mx-2 rounded-md py-1 text-red-900 font-bold justify-end"
+          >About-Us</router-link
+        >
+      </div>
+    </nav>
   </header>
 </template>
 
